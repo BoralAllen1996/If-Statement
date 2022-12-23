1adb38bd59fe3d92d7317a5172a8b3c438c8d826
lenbo <allenbora@gmail.com>
2022-12-23 23:52:58 +0800

    fifteenth

---
+++ If statement.py
@@ -0,0 +1,13 @@
+# Enter a user name here, make sure to make it a string
+
+user_name = "sonnynomnom"    # User name of the person
+
+if user_name == "Dave":
+  print("Get off my computer Allen!")
+
+user_name = "angela_catlady_87"
+
+if user_name == "angela_catlady_87":
+  print("I know it is you, Angela! Go away!")
+  
+#end..........................
